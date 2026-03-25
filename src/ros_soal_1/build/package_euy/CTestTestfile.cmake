bc9e700aac8d21d0bbcc0d9c763791df2f416da9
Albedonald <103135323+Albedonald@users.noreply.github.com>
2026-03-25 17:13:43 +0700

    feat: menambahkan jawaban

---
+++ src/ros_soal_1/build/package_euy/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/derryl/SEKURO_TUGAS_1_Derryl_PROGRAMMING/src/ros_soal_1/src/package_euy
+# Build directory: /home/derryl/SEKURO_TUGAS_1_Derryl_PROGRAMMING/src/ros_soal_1/build/package_euy
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
