732ac5ff2f6b3ab6a4e1ab83e31da5229c1ec71e
Albedonald <103135323+Albedonald@users.noreply.github.com>
2026-03-25 19:25:52 +0700

    Delete src/ros_soal_1/build directory

--- src/ros_soal_1/build/package_euy/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/derryl/SEKURO_TUGAS_1_Derryl_PROGRAMMING/src/ros_soal_1/src/package_euy
-# Build directory: /home/derryl/SEKURO_TUGAS_1_Derryl_PROGRAMMING/src/ros_soal_1/build/package_euy
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
